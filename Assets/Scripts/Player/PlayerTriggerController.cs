0ac99afd94432bb7a9417dabc13b24141834ce26
Lukas <l.schuster1993@googlemail.com>
2016-10-29 08:12:28 +0200

    commit

---
+++ Assets/Scripts/Player/PlayerTriggerController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerTriggerController : MonoBehaviour
+{
+    private const string DOOR_TAG = "Door";
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == DOOR_TAG)
+        {
+            other.GetComponent<DoorBehaviour>().AnimationSlider = 1;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.tag == DOOR_TAG)
+        {
+            other.GetComponent<DoorBehaviour>().AnimationSlider = -1;
+        }
+    }
+}
