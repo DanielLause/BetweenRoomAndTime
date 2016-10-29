03dfc73131ae4a3a587c9e974620bfe51cf07f3e
Daniel Lause <info@daniel-lause.de>
2016-10-29 06:29:49 +0200

    Init Project

---
+++ Assets/Scripts/Player/PlayerGamemanger.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerGamemanger : MonoBehaviour
+{
+    [HideInInspector]
+    public PlayerMovementController PlayerMovementController;
+    [HideInInspector]
+    public PlayerCameraController PlayerCameraController;
+    [HideInInspector]
+    public PlayerWakeUpController PlayerWakeUpController;
+
+    void Awake()
+    {
+        PlayerMovementController = GetComponent<PlayerMovementController>();
+        PlayerCameraController = GetComponent<PlayerCameraController>();
+        PlayerWakeUpController = GetComponent<PlayerWakeUpController>();
+    }
+}
---
+++ Assets/Scripts/TimeShift/DoorBehaviour.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+[RequireComponent(typeof(SphereCollider))]
+class DoorBehaviour : TimeShiftManager
+{
+    public bool DoorBlocked = false;
+    public float TriggerRadius;
+    private SphereCollider collider;
+    void Start()
+    {
+        collider = GetComponent<SphereCollider>();
+        collider.radius = TriggerRadius;
+    }
+    void Update()
+    {
+        if (!DoorBlocked)
+        OnUpdate();
+    }
+
+}
